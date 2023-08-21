528f95459f85815adc72ee22a653dc4f9da98315
Godwin Vaz <vazgodwin10@gmail.com>
2023-08-22 02:20:01 +0530

    changes

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ public class Main {
         System.out.print("Hello and welcome!");
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
-            System.out.println("Hello");
         }
     }
 }
