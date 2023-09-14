0ed20fb32892a6366b81c0aa5fbc7a5dcdf979d0
Godwin Vaz <vazgodwin10@gmail.com>
2023-09-15 02:22:14 +0530

    commiting from bash

--- src/string/Substring1.java
+++ src/string/Substring1.java
@@ -4,6 +4,6 @@ public class Substring1 {
     public static void main(String[] args) {
         String str= "Hello";
         System.out.println(str.toLowerCase());
-
+        System.out.println(str.charAt(2));
     }
 }
