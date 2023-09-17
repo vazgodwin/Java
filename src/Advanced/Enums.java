1cb9ce96926af46d2c9ced60222d8b701d1498bf
Godwin Vaz <vazgodwin10@gmail.com>
2023-09-17 15:30:24 +0530

    changes

---
+++ src/Advanced/Enums.java
@@ -0,0 +1,7 @@
+package Advanced;
+
+public class Enums {
+    public static void main(String[] args) {
+
+    }
+}
--- src/string/Substring1.java
+++ src/string/Substring1.java
@@ -3,7 +3,7 @@ package string;
 public class Substring1 {
     public static void main(String[] args) {
         String str= "Hello";
-        System.out.println(str.toLowerCase());
+        System.out.println("II".toLowerCase());
         System.out.println(str.charAt(2));
     }
 }
