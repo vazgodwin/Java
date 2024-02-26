993c69378ff7ede75fd8db7675eaceeddc635196
Godwin Vaz <vazgodwin10@gmail.com>
2024-02-27 01:02:39 +0530

    commit

---
+++ src/string/TestStr.java
@@ -0,0 +1,14 @@
+package string;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+public class TestStr {
+    Date date=new Date();
+    public static void main(String[] args) {
+        DateTimeFormatter formatter=DateTimeFormatter.ofPattern("yyyyMMddHHmm");
+
+
+    }
+}
